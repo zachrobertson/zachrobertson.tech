f585e4132cd564c83b240bddea65b94d75a97d8d
Zach Robertson <zachrobertson19@gmail.com>
2022-03-19 17:14:13 -0500

    add options to gatsby-plugin-robots-txt that allow for netlify predeploy review

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,12 @@
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = 'https://zachrobertson.tech',
+  DEPLOU_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV
+} = process.env;
+const isNetlifyProduction = NETLIFY_ENV === 'production';
+const siteURL = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
+
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.zachrobertson.tech",
@@ -11,6 +20,67 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     "gatsby-plugin-react-helmet",
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{userAgent: '*'}],
+            sitemap: 'https://zachrobertsonl.tech/sitemap/sitemap-index.xml'
+          },
+          'branch-deploy': {
+            policy : [{userAgent: '*', disallow: ['/']}],
+            sitemap: 'https://zachrobertsonl.tech/sitemap/sitemap-index.xml',
+            host: null
+          },
+          'deploy-review': {
+            policy: [{userAgent: '*', disallow: ['/']}],
+            sitemap: 'https://zachrobertsonl.tech/sitemap/sitemap-index.xml',
+            host: null,
+          }
+        }
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-feed',
+      options: {
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.nodes.map(node => {
+                return Object.assign({}, node.frontmatter, {
+                  description: node.excerpt,
+                  date: node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + node.frontmatter.slug,
+                  guid: site.siteMetadata.siteUrl + node.frontmatter.slug,
+                  custom_elements: [{ "content:encoded": node.html}]
+                })
+              })
+            },
+            query: `{
+              allMarkdownRemark(
+                sort: { order: DESC, fields: [frontmatter___date]},
+                filter: { frontmatter : { title : { ne : "Upcoming Projects and Blogs" } } }
+              ) {
+                nodes {
+                  excerpt
+                  html
+                  frontmatter {
+                    title
+                    date
+                    slug
+                  }
+                }
+              }
+            }
+            `,
+            output: '/rss.xml',
+            title: "ZACHROBERTSON.tech RSS Feed",
+          }
+        ]
+      }
+    },
     {
       resolve: `gatsby-transformer-remark`,
       options: {

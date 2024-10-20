54b060b69ee15961b2d7e883aea69b8608650c3b
Zachary Robertson <zachrobertson19@gmail.com>
2024-10-20 11:39:38 -0500

    refs #7: replace reference to generateStravaDataFile with generateStravaData

--- src/pages/api/cron/daily.ts
+++ src/pages/api/cron/daily.ts
@@ -1,9 +1,9 @@
-import { generateStravaDataFile } from "@/lib/api";
+import { generateStravaData } from "@/lib/api";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function daily(req: NextApiRequest, res: NextApiResponse): Promise<void> {
     try {
-        await generateStravaDataFile();
+        await generateStravaData();
         res.status(200).json({ message: "Strava data updated" });
     } catch (error) {
         res.status(500).json({ message: 'Internal server error, could not fetch strava data!' });
